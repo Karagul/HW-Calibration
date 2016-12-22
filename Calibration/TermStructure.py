4762b73309a2eebeb1f7df18c531869d9a118071
peter.lin <peter.lin@PL-TP-T430>
2016-12-22 10:09:18 +0000

    Add project files.

---
+++ Calibration/TermStructure.py
@@ -0,0 +1,114 @@
+from QuantLib import *
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas import DataFrame
+from collections import namedtuple
+import math
+
+# global data
+# Here enter the Yield Curve reference Data
+calendar = TARGET()
+todaysDate = Date(30,9,2016);
+Settings.instance().evaluationDate = todaysDate
+settlementDate = Date(30,9,2016);
+
+# build rate helpers
+fixedLegFrequency = Annual
+fixedLegTenor = Period(1,Years)
+fixedLegAdjustment = Unadjusted
+fixedLegDayCounter = Thirty360()
+floatingLegFrequency = Semiannual
+floatingLegTenor = Period(6,Months)
+floatingLegAdjustment = ModifiedFollowing
+
+def ycrv_construct(
+    swap_yrs, swap_rates):
+    swap_maturities = [Period(int(y),Years) for y in swap_yrs]
+    swapHelpers = [ SwapRateHelper(QuoteHandle(SimpleQuote(r/100)),
+                                   m, calendar,
+                                   fixedLegFrequency, fixedLegAdjustment,
+                                   fixedLegDayCounter, Euribor6M())
+                   for r,m in list(zip(swap_rates, swap_maturities)) ]
+    return PiecewiseLinearForward(settlementDate, swapHelpers,
+                                            Thirty360())
+
+def get_spot_rates(
+        yieldcurve, day_count,
+        calendar=TARGET(), months=481):
+    spots = []
+    tenors = []
+    ref_date = yieldcurve.referenceDate()
+    calc_date = ref_date
+    for month in range(0, months):
+        yrs = month/12.0
+        d = calendar.advance(ref_date, Period(month, Months))
+        compounding = Compounded
+        freq = Annual
+        zero_rate = yieldcurve.zeroRate(yrs, compounding, freq)
+        tenors.append(yrs)
+        eq_rate = zero_rate.equivalentRate(
+            day_count,compounding,freq,calc_date,d).rate()
+        spots.append(100*eq_rate)
+    return DataFrame(list(zip(tenors, spots)), 
+                     columns=["Maturities","Curve"], 
+                     index=['']*len(tenors))
+
+def create_swaption_helpers(data, quote_type, index, term_structure, engine):
+    swaptions = []
+    fixed_leg_tenor = Period(6, Months)
+    fixed_leg_daycounter = Actual360()
+    floating_leg_daycounter = Actual360()
+    for d in data:
+        vol_handle = QuoteHandle(SimpleQuote(d.volatility)) 
+        if quote_type == 'Normal':
+            helper = SwaptionHelper(Period(int(d.start), Years),
+                                    Period(int(d.length), Years),
+                                    vol_handle,
+                                    index,
+                                    fixed_leg_tenor,
+                                    fixed_leg_daycounter,
+                                    floating_leg_daycounter,
+                                    term_structure,
+                                    CalibrationHelper.RelativePriceError,
+                                    nullDouble(),
+                                    1.0,
+                                    Normal,
+                                    0.0
+                                    )
+        else:
+            helper = SwaptionHelper(Period(int(d.start), Years),
+                                    Period(int(d.length), Years),
+                                    vol_handle,
+                                    index,
+                                    fixed_leg_tenor,
+                                    fixed_leg_daycounter,
+                                    floating_leg_daycounter,
+                                    term_structure,
+                                    CalibrationHelper.RelativePriceError,
+                                    nullDouble(),
+                                    1.0,
+                                    ShiftedLognormal,
+                                    0.0
+                                    )
+        helper.setPricingEngine(engine)
+        swaptions.append(helper)
+    return swaptions
+
+
+def calibrate_hw1f(swaption_quotes, swaption_type, termstructure):
+
+    # calibrate procdure
+    ycrv = YieldTermStructureHandle(termstructure)
+    index = Euribor1Y(ycrv)
+    model = HullWhite(ycrv)
+    engine = JamshidianSwaptionEngine(model)
+    swaptions = create_swaption_helpers(swaption_quotes, swaption_type, index, ycrv, engine)
+
+    optimization_method = LevenbergMarquardt(1.0e-8,1.0e-8,1.0e-8)
+    end_criteria = EndCriteria(10000, 100, 1e-6, 1e-8, 1e-8)
+    model.calibrate(swaptions, optimization_method, end_criteria)
+
+    a, sigma = model.params()
+
+    return a, sigma
---
+++ HW_Calibration.py
@@ -0,0 +1,45 @@
+import xlwings as xw
+from Calibration.TermStructure import *
+import pandas as pd
+import numpy as np
+
+@xw.func
+@xw.arg('x', doc='swap years.')
+@xw.arg('y', doc='swap rates in pencentage points.')
+@xw.ret(index = False, header=False)
+def Bootstrap_TS(x, y):
+    """Returns the annual zero rates"""
+
+    ycrv_swap = ycrv_construct(x,y)
+    splf = get_spot_rates(ycrv_swap, Thirty360(), TARGET(), 1+len(y)*12)
+
+    return splf
+
+
+def Calibrate():
+    # Collect calibration data from Excel
+    sht = xw.Book.caller().sheets['Main']
+    rate_df = pd.DataFrame(sht.range('input_rate').expand().value,
+                           columns=['Term','Rate']) #Expandable rate table
+    swptn_df = pd.DataFrame(sht.range('input_swptn').expand().value,
+                            columns=['Weight','Start','Length','Quote']) #Expandable swaption table
+    
+    #Not yet implemented below, only assume swap rate and Normal swaptions
+    swptn_type = sht.range('type_swptn').value
+    swap_type = sht.range('type_rate').value
+
+    #Call yield construct
+    ycrv_base = ycrv_construct(rate_df['Term'],rate_df['Rate'])
+
+    #Put swaption data into the required format
+    CalibrationData = namedtuple("CalibrationData", 
+                             "start, length, volatility")
+    data_swaptn = [CalibrationData(swptn_df.Start[i], swptn_df.Length[i], swptn_df.Quote[i]) for i in swptn_df.index]
+
+    #Call calibration algorith
+    result = [calibrate_hw1f(data_swaptn, swptn_type, ycrv_base)]
+
+    print(result)
+
+    #Print to Excel
+    sht.range('result_hw1f').value= result
